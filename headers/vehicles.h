79e6c04c553e661cb1a20de60bb2b2dc97bdde1f
Mateus Oliveira <mateusamaraloliveira160302@gmail.com>
2023-03-14 13:38:37 +0000

    feat: 🎸 edit vehicle data
    added a function that allows the vehicle data to be modified (its
    location and current battery)

--- headers/vehicles.h
+++ headers/vehicles.h
@@ -16,3 +16,4 @@ int isUnique(Vehicle *startEntry, int id);
 Vehicle *addVehicles(Vehicle *startEntry, int id, float currentBattery, float autonomy, float price, char brand[], char model[]);
 void listVehicles(Vehicle *startEntry);
 Vehicle *deleteVehicle(Vehicle *startEntry, int id);
+Vehicle *editVehicle(Vehicle *startEntry, int id, int currentBattery, char gpsTracker[]);
