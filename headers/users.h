fba08cfed48703e329b6049c9f930df8a33475d9
Mateus Oliveira <75847514+m0liveira@users.noreply.github.com>
2023-03-15 21:16:50 +0000

    feat: 🎸 add users to database
    added a function that adds users data to the database

--- headers/users.h
+++ headers/users.h
@@ -12,3 +12,4 @@ int isUserUnique(User *startEntry, int id);
 int isNameUnique(User *startEntry, char name[]);
 User *addUser(User *startEntry, int id, char name[], char password[], char role[]);
 int loginUser(User *startEntry, char name[], char password[]);
+int saveUsersOnDatabase(User *startEntry);
